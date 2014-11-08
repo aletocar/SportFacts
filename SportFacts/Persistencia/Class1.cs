ae2c3447a7c7801cabd9dcd5c63b3e62b2694cc0
aletocar <aletocar@hotmail.com>
2014-11-08 11:01:01 -0200

    Persistencia

--- SportFacts/Persistencia/Class1.cs
+++ SportFacts/Persistencia/Class1.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Persistencia
 {
-    public class Class1
+    public class ContextoSportsFacts:DbContext
     {
+        public DbSet<Alimento> Alimentos { get; set; }
+        public DbSet<Deportista> Deportistas { get; set; }
+        public DbSet<Ingesta> Ingestas { get; set; }
+        public DbSet<PlanDietareo> PlanesDietareos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder mb)
+        {
+            mb.Entity<Deportista>()
+                .HasMany(e => e.ListaPlan)
+                .WithMany(
+            
+            base.OnModelCreating(mb);
+        }
+
     }
 }
